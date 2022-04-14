f85e4addc7f43336b445c00b6f86f8ce7872241e
GenZy <87565850+NXTGenZy@users.noreply.github.com>
2022-04-14 12:25:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var first_name = "Chaitanya";
+var last_name = "Shah";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 26;
+var b = 30;
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var sub = b - a;
+console.log(sub);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = b / a;
+console.log(divide);
